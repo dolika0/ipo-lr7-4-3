af008718edfa218130f1fd95dae6de021cadc472
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-12-08 12:24:12 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 import json 
-
 with open("cars.json", 'r', encoding = 'utf-8') as file: 
     data = json.load(file) # Перевод из json в python object
 
+def validation(prompt,input_type = int):
+    while(True):
+        num = input(prompt)
+        if num.isdigit():
+            return int(num)
+        else: 
+            print("Введите необходимую информацию числом!")
+            
+        
 start = True
 count = 0
+
+
 def menu():
     print("""
        1: Вывести все записи 
@@ -29,16 +39,9 @@ def all():
     count += 1
 
 
-
 def index():
     global count
-    while True:
-        id = input("Введите номер записи: ")
-        if id.isdigit():
-            id = int(id)
-            break
-        else: 
-            print("Введите номер записи цифрой!")
+    id = validation("Введите номер записи для вывода: ")
         
     find = False
     for car in data:
@@ -52,66 +55,44 @@ def index():
             Объем бака: {car['tank_volume']}
             """)
             find = True
+            count += 1
             break
-        count += 1
-        
-
-    else: 
+    if find == False: 
         print("\n"," Запись не найдена ".center(60, "~"))
+
 
 def new():
     global count
-    while True:
-        id = input("Введите номер записи машины: ")
-        if id.isdigit():
-            id = int(id)
-            break
-        else: 
-            print("Введите номер записи машины цифрой!")
-        
+    if data:
+        id = max(int(car['id']) for car in data) + 1 
+    else: id = 1
+    
     find = False
     for car in data:
         if car['id'] == id:
             find = True
             break
-        
     if find: 
         print("Такой номер уже существует.")
-
     else:
         new_name = input("Введите название модели: ")  
         new_manufacturer  = input("Введите производителя: ")
 
         while True:
-            numb = input("Введите, заправляется ли бензином (1 - да/ 2 - нет): ")
-            if numb.isdigit():
-                numb = int(numb)
-                if numb == 1:
-                    new_is_petrol = 'да'
-                    break
-                elif numb == 2:
-                    new_is_petrol = 'нет'
-                    break
-                else :
-                    print("Вы должны ввести число!(1 - заправляется бензином, 2 - не заправляется)")
-            else : 
-                print("Введите число (1 или 2)!")
+            num = validation("Введите, заправляется ли бензином (1 - да, 2 - нет): ")
+            if num == 1 or num == 2:
+                new_is_petrol = True if num == 1 else False 
+                break 
+            else:
+                print("Введите 1, если заправляется бензином. Если не заправляется, введите 2.")
 
-
-        while True:
-            new_tank_volume = input("Введите объём бака(целым числом): ")
-            if new_tank_volume.isdigit():
-                new_tank_volume = int(new_tank_volume)
-                break
-            else: 
-                print("Введите объём бака целым числом!")  
-                    
+        new_tank_volume = validation("Введите объём бака(целым числом): ")
 
         new_car = {
             'id': id,
             'name': new_name,
             'manufacturer':  new_manufacturer,
-            'is_petrol': True if new_is_petrol.lower() == 1 else False, 
+            'is_petrol': new_is_petrol, 
             'tank_volume':  new_tank_volume
         }
 
@@ -123,16 +104,9 @@ def new():
 
 def del_id():
     global count
-    while True:
-        id = input("Введите номер записи для удаления: ")
-        if id.isdigit():
-            id = int(id)
-            break
-        else: 
-            print("Введите номер записи для удаления цифрой!")
+    id = validation("Введите номер записи для удаления : ")
 
     find = False  
-
     for car in data:
         if id == car['id']:
             data.remove(car) # Удаление 
@@ -155,15 +129,8 @@ def exit():
 def main():
     while start:
         menu()
-
-        while True:
-            num = input("Введите номер действия: ")
-            if num.isdigit():
-                num = int(num)
-                break
-            else: 
-                print("Введите число!(цифрой)")
-
+    
+        num = validation("Введите номер действия: ")
         if num == 1:
             all()
         elif num == 2:
